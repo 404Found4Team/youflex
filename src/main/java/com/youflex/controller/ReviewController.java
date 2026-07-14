60d947be9d6080f47ba127277ceacea79f6a2918
Ungjo <ungjo911@gmail.com>
2026-07-14 15:02:50 +0900

    리뷰 관련 인터페이스 @Mapper 어노테이션 추가

--- src/main/java/com/youflex/controller/ReviewController.java
+++ src/main/java/com/youflex/controller/ReviewController.java
@@ -1,11 +1,30 @@
 package com.youflex.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import com.youflex.service.ReviewService;
+
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
 public class ReviewController {
-
+	
+	private final ReviewService reviewService;
+	
+//	작성 폼으로 이동
+	@GetMapping("/post/write")
+	public String writeForm(HttpSession session, Model model) {
+//		세션에 loginMember가 없으면 => 로그인 페이지로 이동
+		if(session.getAttribute("loginMember") == null) {
+			return "redirect:/member/login";
+		}
+		
+		return "review/write";
+	}
+	
+	
 }
