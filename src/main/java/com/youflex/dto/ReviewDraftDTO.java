5ed8dfc77576a91c327a708794cb7f4c6131f67c
Ungjo <ungjo911@gmail.com>
2026-07-20 11:10:20 +0900

    ReviewDraft 수정중

--- src/main/java/com/youflex/dto/ReviewDraftDTO.java
+++ src/main/java/com/youflex/dto/ReviewDraftDTO.java
@@ -15,11 +15,7 @@ import lombok.NoArgsConstructor;
 public class ReviewDraftDTO {
     private int reviewDraftId;
     private int memberId;
-    private List<Integer> genreCategoryIds;
     private String reviewDraftTitle;
     private String reviewDraftContent;
     private LocalDateTime reviewDraftSavedAt;
-
-    // 여러 장르명이 한 번에 넘어올 수 있도록 List로 구현
-    private List<String> genreCategoryNames;
 }
