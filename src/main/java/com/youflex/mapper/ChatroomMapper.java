9c3be58f6dc721bf833cf79b46a5c51f47b19b70
Nayoung <gimn13828@gmail.com>
2026-07-21 16:00:56 +0900

    2026/07/21 PM 16:00, 채팅 수신

--- src/main/java/com/youflex/mapper/ChatroomMapper.java
+++ src/main/java/com/youflex/mapper/ChatroomMapper.java
@@ -1,11 +1,15 @@
 package com.youflex.mapper;
+
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import com.youflex.dto.ChatroomDTO;
+
 @Mapper
 public interface ChatroomMapper {
+
     int createChatroom(ChatroomDTO chatroom);
+
     ChatroomDTO selectChatroomById(int chatroomId);
 
     /**
@@ -15,10 +19,25 @@ public interface ChatroomMapper {
     List<ChatroomDTO> selectAllChatrooms(@Param("memberId") Integer memberId);
 
     int updateChatroom(ChatroomDTO chatroom);
+
     int deleteChatroom(int chatroomId);
+
     /**
      * 특정 회원이 개설한 채팅방 개수 조회
      * - 1개 이상이면 이미 개설한 방이 있는 것으로 판단
      */
     int countChatroomByMemberId(int memberId);
+
+    int chatChatroomByChatId(int chatroomId);
+
+    // ---- 입장/나가기 (chatroom_member 테이블) ----
+
+    /** 이미 참여 중인지 확인 (0이면 미참여) */
+    int countChatroomMember(@Param("chatroomId") int chatroomId, @Param("memberId") int memberId);
+
+    /** 채팅방 참여 기록 INSERT */
+    int insertChatroomMember(@Param("chatroomId") int chatroomId, @Param("memberId") int memberId);
+
+    /** 채팅방 참여 기록 DELETE (나가기) */
+    int deleteChatroomMember(@Param("chatroomId") int chatroomId, @Param("memberId") int memberId);
 }
