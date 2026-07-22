9d0d8d7ea8a7b2cc2e221e22d4de69546aa5d29b
Ungjo <ungjo911@gmail.com>
2026-07-22 10:42:27 +0900

    리스트 관련 파일 생성

---
+++ src/main/java/com/youflex/service/ReviewListService.java
@@ -0,0 +1,11 @@
+package com.youflex.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ReviewListService {
+
+}
